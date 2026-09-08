939502565a033bd6db894c5b2d2d29263458e959
nnk25 <2023cs088@stu.ucsc.cmb.ac.lk>
2026-09-08 19:34:45 +0530

    frontend removed

--- frontend/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Substitute environment variables in nginx config
-envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Start nginx
-nginx -g 'daemon off;'
